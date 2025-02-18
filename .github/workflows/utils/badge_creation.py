aa580734ecc448de44d57ef29843fbdcdd987b55
Johannes Pietrzyk <johannes.pietrzyk@tu-dresden.de>
2025-02-18 10:23:49 +0100

    .

--- .github/workflows/utils/badge_creation.py
+++ .github/workflows/utils/badge_creation.py
@@ -5,8 +5,18 @@ import sys
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='Generate outcome for the generation step')
   parser.add_argument('--git-sha', help="Git SHA", required=True, type=str, dest='git_sha')
+  parser.add_argument('step', choices=['generation', 'testing-x86', 'testing-aarch64'])
   args = parser.parse_args()
   
+  if args.step == "generation":
+    job_name_prefix = "run-generation-"
+    step_name = "Generate tsl"
+  elif args.step == "testing-x86":
+    job_name_prefix = "run-compile-and-test-x86"
+    step_name = "Run Tests"
+  elif args.step == "testing-aarch64":
+    job_name_prefix = "run-compile-and-test-aarch64"
+    step_name = "Run Tests"
   data_in = sys.stdin.read()  
   data = json.loads(data_in)
   success = True
@@ -14,14 +24,14 @@ if __name__ == "__main__":
     job_git_sha = job.get("head_sha", "<unknown>")
     if job_git_sha == args.git_sha:
       name = job.get("name", "<unknown>")
-      if name.startswith("run-generation-"):
+      if name.startswith(job_name_prefix):
         for step in job["steps"]: 
-          if step["name"] == "Generate tsl":
+          if step["name"] == step_name:
             if step["conclusion"] != "success":
               success = False
             print(f"{name}: {step['conclusion']}", file=sys.stderr)
           
   if success:
-    print(0)
+    exit(0)
   else:
-    print(1)
+    exit(1)
